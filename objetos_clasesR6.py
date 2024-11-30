920365f8016546a43c7cc365ef266fa4476037b0
MiryamElizabeth2006 <157499620+MiryamElizabeth2006@users.noreply.github.com>
2024-11-30 15:50:47 -0500

    Reto 7 clases staticas y @clases

--- objetos_clasesR6.py
+++ objetos_clasesR6.py
@@ -1,4 +1,5 @@
-
+from datetime import datetime
+#Reto 6
 class Auto:
     def __init__(self, marca, modelo, año, kilometraje=0):
         self.marca = marca
@@ -31,10 +32,31 @@ class Auto:
       else: 
        return ("Ya dejame descansar por favor")
 
-      
-mi_auto = Auto("Toyota", "Corolla", 2022)
+#Reto 7
+    @classmethod
+    def crear_auto_toyota(cls, modelo):
+     año_actual = datetime.now().year
+     marca="Toyota"
+     modelo=modelo
+     return cls(marca, modelo, año_actual)
 
-print(mi_auto.mostrar_informacion())
-print(mi_auto.actualizar_kilometraje(50))
-print(mi_auto.realizar_viaje(200000))
-print(mi_auto.estado_auto())
+    @staticmethod
+    def comparar_kilometraje(auto1, auto2):
+      if auto1.kilometraje == auto2.kilometraje:
+       return "Los autos tienen el mismo kilometraje"
+      else:
+       return "Los autos tienen diferente kilometraje"
+
+    @staticmethod
+    def estado_auto(kilometraje):
+      if kilometraje < 20000:
+        return "Estoy como nuevo"
+      elif 20000 <= kilometraje <= 100000:
+        return "Ya estoy usado"
+      else:
+        return "¡Ya déjame descansar por favor!"
+
+    @classmethod
+    def auto_de_segunda(cls, modelo, kilometraje):
+        año_actual = datetime.now().year - 3  # Suponemos que tiene 3 años de antigüedad
+        return cls(marca="Toyota", modelo=modelo, año=año_actual, kilometraje=kilometraje)
